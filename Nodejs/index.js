3f5b656fc47ebf73d9fcdbdca4a475ed4a7538db
vovanbechin <bechin.vovan@gmail.com>
2020-09-29 08:47:04 +0900

    Node Module

--- Nodejs/index.js
+++ Nodejs/index.js
@@ -1,6 +1,13 @@
-console.log("bechin");
+var Mouse = require('./Mouse');
+var Cat = require('./Cat');
 
-function bac(a,b){
-    console.log("bechin test branchebechin");
-};
+var mickey = new Mouse('black');
+var jerry = new Mouse('white');
 
+console.log(mickey);
+console.log(jerry);
+
+var tom = new Cat();
+tom.eat(mickey);
+tom.eat(jerry);
+console.log(tom)
